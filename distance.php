2ef04ed71c03f7f67fb8bb471dffde13294e4643
PeratX <1215714524@qq.com>
2018-10-20 22:49:34 +0800

    Added distance.php

---
+++ distance.php
@@ -0,0 +1,30 @@
+<?php
+
+use iTXTech\SimpleFramework\Console\Logger;
+
+/**
+ * ReverseImageSearchTool
+ * 
+ * Copyright (C) 2018 PeratX
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+include "sf/autoload.php";
+
+Initializer::initTerminal(true);
+
+$hashA = $argv[1];
+$hashB = $argv[2];
+
+Logger::info("Hamming Distance: " . hamming_distance($hashA, $hashB));
